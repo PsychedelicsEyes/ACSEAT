cbcc04060ff730802c670871ba7f4855b830d4d5
PsychedelicsEyes <eyespsychedelics@gmail.com>
2021-09-27 11:14:31 +0200

    first commit

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/account", name="account")
+     */
+    public function account(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('account/account.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+}
