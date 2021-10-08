ab511b15dd6e812774e028afd3eed234972c215f
djino70 <vuillemardjohnny70@gmail.com>
2021-10-08 10:53:33 +0200

    update div relative

--- assets/styles/modules/btn.js
+++ assets/styles/modules/btn.js
@@ -1,6 +1,6 @@
 export * from './btn'
 
-
+const menuBtn_burger = document.getElementById('burger_menu_responsive');
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 
@@ -8,9 +8,11 @@ let menuOpen = false;
 menuBtn.addEventListener('click', () => {
      if(!menuOpen){
      menuBtn.classList.toggle('open');
+     menuBtn_burger.style.display= "block"
      menuOpen = true;
 }else {
           menuBtn.classList.remove('open');
+          menuBtn_burger.style.display= "none"
           menuOpen = false;
      }
 });
