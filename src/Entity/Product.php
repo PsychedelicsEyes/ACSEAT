0291602852b8d8b817e7050c92d93b8c9cdf7164
PsychedelicsEyes <eyespsychedelics@gmail.com>
2021-10-28 15:28:24 +0200

    Clear back

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,9 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Vich\UploaderBundle\Mapping\Annotation\Uploadable;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * @Vich\Uploadable()
  */
 class Product
 {
